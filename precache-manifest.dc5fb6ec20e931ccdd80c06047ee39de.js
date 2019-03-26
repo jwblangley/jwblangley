fc911be26db632fe1b7d816e04b18680009fc90c
James <jwblangley@gmail.com>
2019-03-26 19:21:33 +0000

    Updates

---
+++ precache-manifest.dc5fb6ec20e931ccdd80c06047ee39de.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b554c48126c48861cff8f431638a7baf",
+    "url": "/jwblangley/static/media/linkedIn.b554c481.svg"
+  },
+  {
+    "revision": "bb700bfc7cdf64d92658",
+    "url": "/jwblangley/static/css/main.99be816f.chunk.css"
+  },
+  {
+    "revision": "342519f6c0d291889d6a",
+    "url": "/jwblangley/static/js/runtime~main.37c5fa8f.js"
+  },
+  {
+    "revision": "931d3fea9f192b162b21",
+    "url": "/jwblangley/static/js/2.8aceaf45.chunk.js"
+  },
+  {
+    "revision": "3ea5e1f47e02a878ae34ccb31154a300",
+    "url": "/jwblangley/static/media/jl.3ea5e1f4.png"
+  },
+  {
+    "revision": "d195e6bc97d1c6e7619dd774552441bb",
+    "url": "/jwblangley/static/media/email.d195e6bc.svg"
+  },
+  {
+    "revision": "bb700bfc7cdf64d92658",
+    "url": "/jwblangley/static/js/main.60f4a52a.chunk.js"
+  },
+  {
+    "revision": "7137286db3c9254ace982646907e65a3",
+    "url": "/jwblangley/static/media/twitter.7137286d.svg"
+  },
+  {
+    "revision": "0a3a0978834655eadc804efda1d3a443",
+    "url": "/jwblangley/static/media/github.0a3a0978.svg"
+  },
+  {
+    "revision": "7811e1e00909cf5b7beb25dc7436b602",
+    "url": "/jwblangley/static/media/instagram.7811e1e0.svg"
+  },
+  {
+    "revision": "9602e2aed9cf9eae42c76ff77158f0f4",
+    "url": "/jwblangley/static/media/500px.9602e2ae.svg"
+  },
+  {
+    "revision": "9bdcc145ae106531b670ce845d1a4ad9",
+    "url": "/jwblangley/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jwblangley/precache-manifest.3c93f07fd8b36580a5edfaad6d83293d.js"
+  "/jwblangley/precache-manifest.dc5fb6ec20e931ccdd80c06047ee39de.js"
 );
 
 workbox.clientsClaim();
