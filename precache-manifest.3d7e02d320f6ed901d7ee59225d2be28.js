febc5f3d3765a5af1c421e86c8bb451a93eb3221
James <jwblangley@gmail.com>
2019-03-26 14:58:33 +0000

    Updates

---
+++ precache-manifest.3d7e02d320f6ed901d7ee59225d2be28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jwblangley/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "342519f6c0d291889d6a",
+    "url": "/jwblangley/static/js/runtime~main.37c5fa8f.js"
+  },
+  {
+    "revision": "3fe4a59da3d2918326fe",
+    "url": "/jwblangley/static/js/main.c8a50c9d.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/jwblangley/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "3fe4a59da3d2918326fe",
+    "url": "/jwblangley/static/css/main.1de0c520.chunk.css"
+  },
+  {
+    "revision": "f6a2694eed04c0f33c095cc218e7b9ae",
+    "url": "/jwblangley/index.html"
+  }
+];
