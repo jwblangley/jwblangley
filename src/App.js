bdaa89a7a7dace95ca76171137dda0e98ab5b6d3
James <jwblangley@gmail.com>
2019-03-26 16:43:12 +0000

    Begin design
    Define colours to use.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './jl.png';
-// TODO: Replace with svg.
+
+// Components
+import LogoDisplay from './components/LogoDisplay.js'
+
+// Styling
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            James Langley
-          </p>
-        </header>
+      <div>
+        <LogoDisplay />
       </div>
     );
   }
---
+++ src/components/LogoDisplay.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from '../img/jl.png';
+// TODO: Replace with svg.
+
+class LogoDisplay extends Component {
+  render() {
+    return (
+      <div className="title-header">
+        <img src={logo} className="title-logo" alt="logo" />
+          <div className="title-text">
+          <h1>
+            James Langley
+          </h1>
+          <h2>
+            Computer Scientist and Programmer
+          </h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LogoDisplay;
